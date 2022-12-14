de73eecf60d0898e785dd7535b76b96e81f21604
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-12-14 15:55:08 +0100

    ref

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -5,32 +5,32 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
 
 const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user || !user.verify || !user.comparePassword(password))
-      throw new Unauthorized("Email or password is wrong");
+	try {
+		const { email, password } = req.body;
+		const user = await User.findOne({ email });
+		if (!user || !user.comparePassword(password))
+			throw new Unauthorized("Email or password is wrong");
 
-    const payload = {
-      id: user._id,
-    };
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "5h" });
-    await User.findByIdAndUpdate(user._id, { token });
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        user: {
-          name: user.name,
-          email: user.email,
-          subscription: user.subscription,
-        },
-        token,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+		const payload = {
+			id: user._id,
+		};
+		const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "5h" });
+		await User.findByIdAndUpdate(user._id, { token });
+		res.json({
+			status: "success",
+			code: 200,
+			data: {
+				user: {
+					name: user.name,
+					email: user.email,
+					subscription: user.subscription,
+				},
+				token,
+			},
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 module.exports = login;
