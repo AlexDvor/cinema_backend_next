b0af4f8522b1a465f5f00b5b5be9d5758999c0eb
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-12-28 15:38:06 +0100

    add actor toggle

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,5 +7,6 @@ router.get("/current/user", auth, ctrl.getCurrent);
 router.get("/profile", auth, ctrl.getProfile);
 router.get("/profile/favorites", auth, ctrl.getFavoriteList);
 router.post("/profile/favorite/movies/toggle", auth, ctrl.toggleFavoriteMovie);
+router.post("/profile/favorite/actor/toggle", auth, ctrl.toggleFavoriteActor);
 
 module.exports = router;
